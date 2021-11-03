d93150cbc2525d0ba4d9c49aef999e8ef4d7fc68
Michael B <michaelb@Michaels-MacBook-Pro-2.local>
2021-11-03 15:26:47 -0400

    adding xstate, react router dom & typescript

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from "react";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import LandingPage from "./screens/landing";
+import "./assets/css/App.scss";
+
+const App = () => {
+  return (
+    <HashRouter>
+      <>
+        <Switch>
+          <Route exact path="/" component={LandingPage}></Route>
+        </Switch>
+      </>
+    </HashRouter>
+  );
+};
 
 export default App;
