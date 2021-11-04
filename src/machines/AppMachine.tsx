118d115eb316eb3cf86bc9fddac6a391ef1f23f9
Michael B <michaelb@Michaels-MacBook-Pro-2.local>
2021-11-04 10:36:25 -0400

    adding class for findClientByName and logic to flow through modal

--- src/machines/AppMachine.tsx
+++ src/machines/AppMachine.tsx
@@ -1,60 +1,115 @@
-import {createMachine, assign, DoneEvent} from 'xstate'
-import {  toast } from 'react-toastify';
-import { fetchJson } from '../shared/fetchJson';
+import { createMachine, assign, DoneEvent } from "xstate";
+import { toast } from "react-toastify";
+import { fetchJson } from "../shared/fetchJson";
 
-type AppEvents = GetAllTimeSheetData;
+type AppEvents =
+  | GetAllTimeSheetData
+  | FindByClientName
+  | ClosingModal
+  | ToggleModal;
 
 interface AppMachineContextProps {
-    timeSheetData: any
+  timeSheetData: any;
+  isModalOpen: boolean;
+  modalData?: any;
+  modalTitle: String;
 }
 
 const intialContext: AppMachineContextProps = {
-    timeSheetData: {}
-}
+  timeSheetData: {},
+  isModalOpen: false,
+  modalData: {},
+  modalTitle: "",
+};
 
-const appMachine = createMachine<AppMachineContextProps, AppEvents>({
-    id: 'appMachine',
-    initial:'fetchingData',
+const appMachine = createMachine<AppMachineContextProps, AppEvents>(
+  {
+    id: "appMachine",
+    initial: "fetchingData",
     context: intialContext,
-    states:{
-        idle:{},
-        fetchingData: {
-            invoke:{
-                src:'getAllClientInstances',
-                onDone:{
-                    target: 'idle',
-                    actions: ['setClientInstance', 'flashSuccess']
-                },
-                onError:{
-                    target: 'idle',
-                    actions: 'flashSuccess'
-                }
-            }
-        },
-        failed:{
 
-        }
-    }
-},{
-    services: {
-        getAllClientInstances: () => fetchJson('http://localhost:8080/timesheetApi/getAll')
+    states: {
+      idle: {
+        on: {
+          TOGGLE_MODAL: { actions: "toggleModal" },
+          FIND_BY_CLIENT_NAME: {
+            target: "findingByClient",
+          },
+        },
+      },
+      fetchingData: {
+        invoke: {
+          src: "getAllClientInstances",
+          onDone: {
+            target: "idle",
+            actions: ["setClientInstance", "flashSuccess"],
+          },
+          onError: {
+            target: "idle",
+            actions: "flashSuccess",
+          },
+        },
+      },
+      findingByClient: {
+        invoke: {
+            src: "findClientByName",
+            onDone: {
+              target: "fetchingData",
+              actions: ["toggleModal", "flashSuccess"],
+            },
+            onError: {
+              target: "idle",
+              actions: "flashSuccess",
+            },
+          },
+      },
+      failed: {},
     },
-    actions:{
-        setClientInstance: assign((context, event) => {
-            const e = event as DoneEvent;
-            return{...context, timeSheetData: e.data.clientInstancePojoList}
-        }),
-        flashError: ((context, event) => {
-            const e = event as DoneEvent;
-            toast.error(e.data.statusMessage)
-        }),
-        flashSuccess: ((context, event) => {
-            const e = event as DoneEvent;
-            toast.success(e.data.statusMessage)
-        })
-    }
-})
+  },
+  {
+    services: {
+      getAllClientInstances: () =>
+        fetchJson("http://localhost:8080/timesheetApi/getAll"),
+      findClientByName: (context, event) => {
+        const { request } = event as FindByClientName;
+        return fetchJson("", { method: "POST", body: JSON.stringify(request) });
+      },
+    },
+    actions: {
+      setClientInstance: assign((context, event) => {
+        const e = event as DoneEvent;
+        return { ...context, timeSheetData: e.data.clientInstancePojoList };
+      }),
+      flashError: (context, event) => {
+        const e = event as DoneEvent;
+        toast.error(e.data.statusMessage);
+      },
+      flashSuccess: (context, event) => {
+        const e = event as DoneEvent;
+        toast.success(e.data.statusMessage);
+      },
+      toggleModal: (context, event) => {
+        const e = event as ToggleModal;
+        return {
+          ...context,
+          isModalOpen: !context.isModalOpen,
+          modalData: e.modalData,
+          titile: e.title,
+        };
+      },
+    },
+  }
+);
 
 export default appMachine;
 
-type GetAllTimeSheetData = {type: 'GET_ALL_TIMESHEET_DATA'};
+type GetAllTimeSheetData = { type: "GET_ALL_TIMESHEET_DATA" };
+
+type FindByClientName = {
+  type: "FIND_BY_CLIENT_NAME";
+  request: { clientName: String };
+};
+
+type ClosingModal = { type: "CLOSE" };
+
+type ToggleModal = { type: "TOGGLE_MODAL"; modalData: any; title: String };
